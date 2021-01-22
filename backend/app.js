78fd3afa9c194450bfef67ca7f8ae841037ce095
MeeraMeera1 <gatesameera98@gmail.com>
2021-01-22 16:17:01 -0500

    first commit

---
+++ backend/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const { environment } = require('./config');
+
+const isProduction = environment === 'production';
+
+const app = express();
+//MIDDLEWARES 
+//morgan middleware for logging information about requests and responses
+app.use(morgan('dev'));
+//for parsing cookies 
+app.use(cookieParser());
+//for parsing JSON bodies of requests with content type of "application/json"
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+// helmet helps set a variety of headers to better secure your app
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true,
+        },
+    })
+);
